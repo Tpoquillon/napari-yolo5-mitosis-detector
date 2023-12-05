76dca9f0606db8cdf7476a00a2203b25700c0024
Titouan Etienne Poquillon <titouanp@nus.edu.sg>
2023-12-05 11:41:06 +0800

    Improve loading time and 2d shape display
    Signed-off-by: Titouan Etienne Poquillon <titouanp@nus.edu.sg>

--- src/napari_yolo5_mitosis_detector/core.py
+++ src/napari_yolo5_mitosis_detector/core.py
@@ -1,19 +1,33 @@
 import numpy as np
 import pandas as pd
-import torch
-from torch.hub import load
 from pathlib import Path
 import napari
-model = load('ultralytics/yolov5', 'custom', path=Path(r"D:\Data\Models\Yolo5\mito-nuclei-detection") / "weights" / "best.pt",force_reload=False)
+import threading
+model = None
 
-if torch.cuda.is_available(): 
-    model = model.to(torch.device("cuda"))
+def load_model():
+    import torch
+    from torch.hub import load
+    global model
+    model = load('ultralytics/yolov5', 'custom', path=Path(r"D:\Data\Models\Yolo5\mito-nuclei-detection") / "weights" / "best.pt",force_reload=False)
+    if torch.cuda.is_available():
+        model = model.to(torch.device("cuda"))
+loader = threading.Thread(target=load_model)
+loader.start()
+
+def model_loaded():
+    if model is None:
+        loader.join()
+
+
 def im_preproc(im):
     q99 = np.quantile(im,0.999)
     q01 = np.quantile(im, 0.001)
     return (((im-q01)/(q99-q01)).clip(0,1)*255).astype(np.uint8)
 
 def zxy_to_pandas(im):
+    model_loaded()
+
     batch_size=8
     im = im_preproc(im)
     batches = [im[i:i+batch_size] for i in range(0,len(im),batch_size)]
@@ -27,6 +41,14 @@ def zxy_to_pandas(im):
     return df
 def xy_to_rectangle(im:np.ndarray):
     assert len(im.shape)==2 , 'img should have 2 dimention'
+    lay =  napari.layers.Shapes(ndim=2)
     df = zxy_to_pandas(im[None,...])
-    rectangle_data = np.array([[[row.ymin,row.xmin],[row.ymax,row.xmax]] for _,row in df.iterrows()])
-    return napari.layers.Shapes(data=rectangle_data,ndim=2,edge_color="red")
+
+    rectangle_data_mito = np.array([[[row.ymin,row.xmin],[row.ymax,row.xmax]] for _,row in df.iterrows() if row["class"]==0])
+    if len(rectangle_data_mito>0 ):
+        lay.add_rectangles((rectangle_data_mito),edge_width=4, edge_color="green", face_color="#ffffff32", z_index=2)
+    
+    rectangle_data_nuc = np.array([[[row.ymin,row.xmin],[row.ymax,row.xmax]] for _,row in df.iterrows() if row["class"]==1])
+    if len(rectangle_data_nuc>0 ):
+        lay.add_rectangles((rectangle_data_nuc),edge_width=2, edge_color="red", face_color="#ffffff32", z_index=1)
+    return lay
